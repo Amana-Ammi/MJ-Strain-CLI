3e7e962555ffba1a9f592a90e9a572763d61cee5
Amonsta <amana.ammi@gmail.com>
2020-07-09 22:37:00 -0600

    age verify changes

--- lib/models/cli.rb
+++ lib/models/cli.rb
@@ -1,4 +1,6 @@
 require 'pry'
+require_relative "../models/strain.rb"
+require_relative "../services/api.rb"
 
 class CLI 
     attr_accessor :age, :user_input
@@ -6,10 +8,68 @@ class CLI
     def start
         age_verification
         welcome
+        how_to_exit
         #in a rush? here's a random strain?
         # Some kind of menu w/options for search by flavor, effects, or strain
         # Commands for user to continue in program and also exit when done
         # A thank you/goodbye message. Please endulge responsibly
+    end
+
+    def user_input
+        user_input = gets.chomp
+    end 
+
+    def goodbye
+        puts ""
+        puts "Thank you for your visit!"
+        puts ""
+        puts "Please enjoy responsibly!"
+        puts ""
+        exit_program
+    end
+
+    def how_to_exit
+        puts ""
+        puts "                  If you'd like to exit the program, please type 'Q' at anytime."
+        puts ""
+        if user_input == "Q"
+            exit_program
+        end
+    end
+
+    def exit_program
+        exit 
+    end
+
+    def user_exit
+        if user_input == "exit"
+            exit
+        end
+    end
+
+    def age_verification
+        puts ""
+        puts "Before we get started, please enter your age below."
+            if user_input.to_i >= 21
+                puts "         ~~~ Congratulations! You were born just in time to use this application!! ~~~"
+                puts ""
+            # elsif user_input.to_i < 21
+            #     puts "Looks like you're too young to participate. Please come back when you're of legal age! :)"
+            #     puts ""
+            #     exit_program
+            # elsif user_input == user_input
+            #     puts ""
+            #     puts "Invalid input. Please try again using only numbers."
+            #     puts ""
+            #     age_verification
+            end 
+    end
+
+    def main_menu
+    end
+
+    def hurry_up
+        Strain.all
     end
 
     def welcome
@@ -27,32 +87,9 @@ class CLI
     WWWW | WWWW                                                                  WWWW | WWWW 
          |                                                                            | 
          |                                                                            | "
+
     end
 
-    def goodbye
-    end
-
-    def exit_program
-        exit 
-    end
-
-    def user_exit
-        if user_input == "exit"
-            exit
-        end
-    end
-
-    def age_verification
-        puts "Before we get stared, please enter your age below."
-        user_input = gets.chomp
-  
-        if user_input.to_i >= 21
-            "Congratulations! You were born just in time to use this application!!"
-        else 
-            puts "Looks like you're too young to participate. Please come back when you're of legal age! :)"
-            exit_program
-        end 
-    end
   binding.pry
 end
 
