20c691624cc8fab76074f2bb9074f5df778e1b5d
Amonsta <amana.ammi@gmail.com>
2020-07-09 18:39:02 -0600

    cli changes

--- lib/models/cli.rb
+++ lib/models/cli.rb
@@ -1,35 +1,57 @@
 require 'pry'
 
 class CLI 
-    attr_accessor :age
+    attr_accessor :age, :user_input
 
     def start
+        age_verification
         self.welcome
-        # Age verification
-        # Populate the random flavor of the moment
+        #in a rush? here's a random strain?
         # Some kind of menu w/options for search by flavor, effects, or strain
         # Commands for user to continue in program and also exit when done
-        # A thank you/goodbye message
+        # A thank you/goodbye message. Please endulge responsibly
     end
 
     def welcome
-        "Hello, and Welcome to The Strain You Never Knew You Wanted to Try!"
+        puts "
+         W                                                                            W   
+        WWW                                                                          WWW   
+        WWW                                                                          WWW   
+       WWWWW                                                                        WWWWW 
+ W     WWWWW     W                                                            W     WWWWW     W 
+ WWW   WWWWW   WWW    Hello!! Welcome to what could be your best day ever!    WWW   WWWWW   WWW  
+  WWW  WWWWW  WWW                                                              WWW  WWWWW  WWW 
+   WWW  WWW  WWW                                                                WWW  WWW  WWW 
+    WWW WWW WWW                                                                  WWW WWW WWW  
+      WWWWWWW                                                                      WWWWWWW 
+    WWWW | WWWW                                                                  WWWW | WWWW 
+         |                                                                            | 
+         |                                                                            | "
     end
 
-    def age_verification(age)
-        puts "Before we get stared, please enter your age below."
-        age = gets.chomp
+    def goodbye
+    end
 
-        if age >= 21
+    def exit_program
+        
+    end
+
+    
+
+    def age_verification
+        puts "Before we get stared, please enter your age below."
+        user_input = gets.chomp.to_i
+  
+
+        if user_input >= 21
             puts "Congratulations! You were born just in time to use this application!!"
         else 
             puts "Looks like you're too young to participate. Please come back in a couple years!"
+            #exit program
         end 
     end
- 
-
-
+  binding.pry
 end
 
-x =CLI.new
+x = CLI.new
 x.start
