7e8f8e5b7301fa4f5e9e3158abfe1893f52d9344
Amonsta <amana.ammi@gmail.com>
2020-07-09 20:40:02 -0600

    cli changes

--- lib/models/cli.rb
+++ lib/models/cli.rb
@@ -5,7 +5,7 @@ class CLI
 
     def start
         age_verification
-        self.welcome
+        welcome
         #in a rush? here's a random strain?
         # Some kind of menu w/options for search by flavor, effects, or strain
         # Commands for user to continue in program and also exit when done
@@ -33,21 +33,24 @@ class CLI
     end
 
     def exit_program
-        
+        exit 
     end
 
-    
+    def user_exit
+        if user_input == "exit"
+            exit
+        end
+    end
 
     def age_verification
         puts "Before we get stared, please enter your age below."
-        user_input = gets.chomp.to_i
+        user_input = gets.chomp
   
-
-        if user_input >= 21
-            puts "Congratulations! You were born just in time to use this application!!"
+        if user_input.to_i >= 21
+            "Congratulations! You were born just in time to use this application!!"
         else 
-            puts "Looks like you're too young to participate. Please come back in a couple years!"
-            #exit program
+            puts "Looks like you're too young to participate. Please come back when you're of legal age! :)"
+            exit_program
         end 
     end
   binding.pry
