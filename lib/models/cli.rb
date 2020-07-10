bd156718a38eae06bb7c0f559958c1c94967ef5a
Amonsta <amana.ammi@gmail.com>
2020-07-10 10:35:54 -0600

    spacing changes

--- lib/models/cli.rb
+++ lib/models/cli.rb
@@ -1,6 +1,6 @@
-require 'pry'
-require_relative "../models/strain.rb"
-require_relative "../services/api.rb"
+# require 'pry'
+# require_relative "../models/strain.rb"
+# require_relative "../services/api.rb"
 
 class CLI 
     attr_accessor :age, :user_input
@@ -13,6 +13,7 @@ class CLI
         # Some kind of menu w/options for search by flavor, effects, or strain
         # Commands for user to continue in program and also exit when done
         # A thank you/goodbye message. Please endulge responsibly
+        goodbye
     end
 
     def user_input
@@ -21,9 +22,9 @@ class CLI
 
     def goodbye
         puts ""
-        puts "Thank you for your visit!"
+        puts "                                  Thank you for your visit!"
         puts ""
-        puts "Please enjoy responsibly!"
+        puts "                                ~~ Please enjoy responsibly! ~~"
         puts ""
         exit_program
     end
@@ -33,6 +34,7 @@ class CLI
         puts "                  If you'd like to exit the program, please type 'Q' at anytime."
         puts ""
         if user_input == "Q"
+            goodbye
             exit_program
         end
     end
@@ -53,10 +55,10 @@ class CLI
             if user_input.to_i >= 21
                 puts "         ~~~ Congratulations! You were born just in time to use this application!! ~~~"
                 puts ""
-            # elsif user_input.to_i < 21
-            #     puts "Looks like you're too young to participate. Please come back when you're of legal age! :)"
-            #     puts ""
-            #     exit_program
+            elsif user_input.to_i < 21
+                puts "Looks like you're too young to participate. Please come back when you're of legal age! :)"
+                puts ""
+                exit_program
             # elsif user_input == user_input
             #     puts ""
             #     puts "Invalid input. Please try again using only numbers."
@@ -71,6 +73,7 @@ class CLI
     def hurry_up
         Strain.all
     end
+    
 
     def welcome
         puts "
@@ -90,8 +93,20 @@ class CLI
 
     end
 
-  binding.pry
+#   binding.pry
 end
 
 x = CLI.new
 x.start
+
+
+
+# def age_verification
+#     puts "Before we get stared, please enter your age below."
+#     user_input = gets.chomp
+
+#     if user_input.to_i >= 21
+#         "Congratulations! You were born just in time to use this application!!"
+#     else 
+#         puts "Looks like you're too young to participate. Please come back when you're of legal age! :)"
+#         exit_program
